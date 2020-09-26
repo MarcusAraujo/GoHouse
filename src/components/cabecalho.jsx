803aae2063e0b15b496428e22e47ab2ac6269574
Marcus Araújo <marcusmv1996@gmail.com>
2020-09-26 18:02:58 -0300

    logo
    logo inserida

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Menu from './menu'
-import imageLogo from '../components/imagem'
+import imageLogo from '../components/logo.png'
 
 export default props => <div>
     <img src={imageLogo} alt='Logo da aplicação' />
