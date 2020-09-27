6e7a67fb6e6a05010459137edab1324d294b016d
Marcus Araújo <marcusmv1996@gmail.com>
2020-09-27 12:25:47 -0300

    err
    body

---
+++ src/components/body.jsx
@@ -0,0 +1,8 @@
+/*import React from 'react'
+import imageLogo from '../components/logo.png'
+
+
+export default props => <div>
+    <img src={imageLogo} alt='Logo da aplicação' />
+    
+</div>*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Head from './components/head'
+import Head from './components/head';
+import Body from './components/body'
 
 ReactDOM.render(
   
-  <Head></Head>,
+  <Head></Head>,//<Body></Body>,
   document.getElementById('root')
 );
 
